e6b91ebaa5ea3b6690ad12635a467900a8871c18
Shikma Lerner - Friedman <shikma19@meet.mit.edu>
2018-11-07 21:38:40 +0200

    Lots of amazing changes

--- databases.py
+++ databases.py
@@ -10,16 +10,25 @@ session = DBSession()
 
 # Write your functions to interact with the database here :
 
-def create_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def create_product(name,price,quantity,description,):
+	p = Product(name =name ,price = price ,quantity = quantity,description = description )
+	session.add(p)
+	session.commit()
 
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
 
+def update_product(id,new_quantity):
+ 	p = session.query(Product).filter_by(id = id ).first()
+ 	p.quantity = new_quantity
+ 	session.commit()
 def delete_product(id):
-  pass
+		p = session.query(Product).filter_by(id = id).delete()
+		session.commit()  
 
 def get_product(id):
-  pass
+	p = session.query(Product).filter_by(id = id).first()
+	return p
+
+def first_product(id):
+	return get_product(id)
+
+print(first_product(1))
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -7,5 +7,10 @@ Base = declarative_base()
 
 # Write your classes here :
 class Product(Base):
-    # TODO: complete this class
-    pass
+	__tablename__ = 'products'
+	id = Column(Integer,primary_key = True)
+	name = Column(String)
+	price = Column(Float)
+	quantity = Column(Float)
+	description = Column(String)
+   
